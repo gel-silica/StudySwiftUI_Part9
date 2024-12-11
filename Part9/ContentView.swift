82cffcbaaa10e374f3aa9fa049279207179880f7
silicagel <ksk19981004@gmail.com>
2024-12-12 06:47:16 +0900

    dialog

--- Part9/ContentView.swift
+++ Part9/ContentView.swift
@@ -9,22 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isShowAlert = false
+    @State var isShowDialog = false
     var body: some View {
         VStack {
             Button("アラート") {
                 isShowAlert = true
             }
             .padding()
+            
+            Button("ダイアログ") {
+                isShowDialog = true
+            }
         }
         .padding()
         .alert("本当に削除しますか？", isPresented: $isShowAlert) {
             Button("削除する", role: .destructive) {
                 
             }
-            Button("キャンセル", role: .cancel) {
+            Button("キャンセル", role: .cancel) { }
+        } message: { Text("一度削除したら元に戻すことはできません")
+        }
+        .confirmationDialog("タイトル", isPresented: $isShowDialog, titleVisibility: .visible) {
+            Button("選択肢1"){
                 
             }
-        } message: { Text("一度削除したら元に戻すことはできません") }
+            Button("選択肢2"){
+                
+            }
+            Button("選択肢3"){
+                
+            }
+            Button("選択肢4"){
+                
+            }
+            Button("キャンセル", role: .cancel){
+                
+            }
+        } message: {
+            Text("ここにメッセージ")
+        }
     }
 }
 
