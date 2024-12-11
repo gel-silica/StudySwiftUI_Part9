7384ca3583e137616c624db2713b900a9afc4426
silicagel <ksk19981004@gmail.com>
2024-12-12 06:29:43 +0900

    Initial Commit

---
+++ Part9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Part9
+//
+//  Created by silicagel on 2024/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Part9/Part9App.swift
@@ -0,0 +1,17 @@
+//
+//  Part9App.swift
+//  Part9
+//
+//  Created by silicagel on 2024/12/12.
+//
+
+import SwiftUI
+
+@main
+struct Part9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
