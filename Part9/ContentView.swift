2ff30554830070f0ef9af015d901930aa38f5b89
silicagel <ksk19981004@gmail.com>
2024-12-12 06:38:31 +0900

    alert

--- Part9/ContentView.swift
+++ Part9/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShowAlert = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("アラート") {
+                isShowAlert = true
+            }
+            .padding()
         }
         .padding()
+        .alert("本当に削除しますか？", isPresented: $isShowAlert) {
+            Button("削除する", role: .destructive) {
+                
+            }
+            Button("キャンセル", role: .cancel) {
+                
+            }
+        } message: { Text("一度削除したら元に戻すことはできません") }
     }
 }
 
